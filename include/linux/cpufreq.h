233970a3a5e402cc3f1290ddc529e375cf4b005f
MobiusM <danholandi@gmail.com>
2016-07-26 00:49:07 +0300

    Remove intellidemand2 governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -396,9 +396,6 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND2)
-extern struct cpufreq_governor cpufreq_gov_intellidemand2;
-#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellidemand2)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellimm)
